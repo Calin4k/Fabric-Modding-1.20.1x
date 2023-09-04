24690d7a973b0ca8cc3c47a7d41ffb41907891c6
Golden <Ndelohcalin@gmail.com>
2023-09-04 16:35:44 -0400

    Initial commit

---
+++ src/main/java/net/nick/nickmod/NickModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nick.nickmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NickModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/nick/nickmod/NickmodClient.java
@@ -0,0 +1,10 @@
+package net.nick.nickmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NickmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
